6aad3d63d87726a4f3206671101fb2ee5a47f1a3
davidcoats <david@getsafetycone.com>
2022-06-09 12:22:03 -0700

    Interim changes.

--- source/R5T.D0097.I001/Code/Bases/Extensions/IServiceActionExtensions.cs
+++ source/R5T.D0097.I001/Code/Bases/Extensions/IServiceActionExtensions.cs
@@ -10,7 +10,7 @@ namespace R5T.D0097.I001
     public static class IServiceActionExtensions
     {
         /// <summary>
-        /// Adds the <see cref="TypeBasedJsonReserializer{TBase}"/> implementation of <see cref="ITypeBasedJsonReserializer{TBase}"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// Adds the <see cref="TypeBasedJsonReserializer{TBase}"/> implementation of <see cref="ITypeBasedJsonReserializer{TBase}"/> as a <see cref="Microsoft.Extensions.DependencyInjection.ServiceLifetime.Singleton"/>.
         /// /// Note: the <see cref="TypeBasedJsonReserializer{TBase}"/> service should not be used directly, but should be sub-classed (or used a as "has-a" component) in a class specific to the <typeparamref name="TBase"/> type.
         /// </summary>
         public static IServiceAction<ITypeBasedJsonReserializer<TBase>> AddTypeBasedJsonReserializerAction<TBase>(this IServiceAction _,
